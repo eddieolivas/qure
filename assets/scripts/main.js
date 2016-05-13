c45bdd10207a697700881b5f2443b2f60195137b
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2016-05-13 13:55:50 -0700

    Making animation scripts only load on home page.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -18,44 +18,44 @@
     // All pages
     'common': {
       init: function() {
-        var $window           = $(window),
-        win_height_padded = $window.height() * 1.1;
-      function revealOnScroll() {
-        var scrolled = $window.scrollTop(),
-            win_height_padded = $window.height() * 1.1;
-
-        // Showed...
-        $(".revealOnScroll:not(.animated)").each(function () {
-          var $this     = $(this),
-              offsetTop = $this.offset().top;
-
-          if (scrolled + win_height_padded > offsetTop) {
-            if ($this.data('timeout')) {
-              window.setTimeout(function(){
-                $this.addClass('animated ' + $this.data('animation'));
-              }, parseInt($this.data('timeout'),10));
-            } else {
-              $this.addClass('animated ' + $this.data('animation'));
-            }
-          }
-        });
-        // Hidden...
-       $(".revealOnScroll.animated").each(function (index) {
-          var $this     = $(this),
-              offsetTop = $this.offset().top;
-          if (scrolled + win_height_padded < offsetTop) {
-            $(this).removeClass('animated fadeInUp flipInX lightSpeedIn');
-          }
-        });
-      }
-
-      //revealOnScroll();
-
-      $window.on('scroll', revealOnScroll);
       
       },
       finalize: function() {
         // JavaScript to be fired on all pages, after page specific JS is fired
+        var $window           = $(window),
+        win_height_padded = $window.height() * 1.1;
+        function revealOnScroll() {
+          var scrolled = $window.scrollTop(),
+              win_height_padded = $window.height() * 1.1;
+
+          // Showed...
+          $(".revealOnScroll:not(.animated)").each(function () {
+            var $this     = $(this),
+                offsetTop = $this.offset().top;
+
+            if (scrolled + win_height_padded > offsetTop) {
+              if ($this.data('timeout')) {
+                window.setTimeout(function(){
+                  $this.addClass('animated ' + $this.data('animation'));
+                }, parseInt($this.data('timeout'),10));
+              } else {
+                $this.addClass('animated ' + $this.data('animation'));
+              }
+            }
+          });
+          // Hidden...
+         $(".revealOnScroll.animated").each(function (index) {
+            var $this     = $(this),
+                offsetTop = $this.offset().top;
+            if (scrolled + win_height_padded < offsetTop) {
+              $(this).removeClass('animated fadeInUp flipInX lightSpeedIn');
+            }
+          });
+        }
+
+        //revealOnScroll();
+
+        $window.on('scroll', revealOnScroll);
       }
     },
     // Home page
