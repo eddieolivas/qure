47d7423e46430ee3c2974b033dd0d8686ff31007
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2016-05-13 18:32:05 -0700

    Trying to modify animation speed.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -23,10 +23,10 @@
       finalize: function() {
         // JavaScript to be fired on all pages, after page specific JS is fired
         var $window           = $(window),
-        win_height_padded = $window.height() * 1.1;
+        win_height_padded = $window.height() * 1.5;
         function revealOnScroll() {
           var scrolled = $window.scrollTop(),
-              win_height_padded = $window.height() * 1.1;
+              win_height_padded = $window.height() * 1.5;
 
           // Showed...
           $(".revealOnScroll:not(.animated)").each(function () {
